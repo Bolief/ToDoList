79b01ca6fdacb1a9a911e3341d389909e7adaf63
francis.bolie <francis.bolie@5CD2452PWK>
2024-10-20 20:57:11 -0500

    Add project files.

---
+++ ToDoList/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoItem>().ToTable("ToDoItems");
+
+            // Seed data
+            modelBuilder.Entity<ToDoItem>().HasData(
+                new ToDoItem { Id = 1, Title = "Sample Task 1", IsCompleted = false },
+                new ToDoItem { Id = 2, Title = "Sample Task 2", IsCompleted = true }
+            );
+        }
+
+    }
+}
---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
